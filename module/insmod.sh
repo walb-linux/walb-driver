deb449e7d958c827d270f6d0ab464f9e6ca55587
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2010-11-15 04:50:06 +0000

    modify insmod for new test environment.

--- module/insmod.sh
+++ module/insmod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo dmesg -n7
-sudo insmod walb.ko ddev_major=9 ddev_minor=10 ldev_major=9 ldev_minor=11 ndevices=1
+sudo insmod walb.ko ddev_major=8 ddev_minor=0 ldev_major=8 ldev_minor=16 ndevices=1
