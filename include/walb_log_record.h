5f457af9dd244865ed7be61321d5ccb90d7e806d
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2010-12-02 01:41:00 +0000

    add a bit detail comment.

--- include/walb_log_record.h
+++ include/walb_log_record.h
@@ -47,7 +47,8 @@ typedef struct walb_log_record {
 typedef struct walb_logpack_header {
 
         u32 checksum; /* checksum of whole log pack. */
-        u16 n_records; /* Number of log records in the log pack. */
+        u16 n_records; /* Number of log records in the log pack.
+                          This includes padding records also. */
         u16 total_io_size; /* Total io size in the log pack
                               [physical sector].
                               (Log pack size is total_io_size + 1.) */
