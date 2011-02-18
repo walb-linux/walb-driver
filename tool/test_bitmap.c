22916590fc2adb0c1e1171d32fcaa304c0f82374
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2011-02-18 11:45:57 +0900

    update test_bitmap.c

--- tool/test_bitmap.c
+++ tool/test_bitmap.c
@@ -5,62 +5,89 @@
  * @author HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
  * @license 3-clause BSD, GPL version 2 or later.
  */
-#include <assert.h>
 #include <stdio.h>
 
 #include "bitmap.h"
-
 
 void test(int size)
 {
         int i;
         walb_bitmap_t *bmp;
-        int ret;
 
+        printf("test with size %d...\n", size);
+
+        /* Create a bitmap. */
         bmp = walb_bitmap_create(size);
+
+        for (i = 0; i < size; i ++) {
+                ASSERT(! walb_bitmap_get(bmp, i));
+        }
+        ASSERT(walb_bitmap_is_all_off(bmp));
+
+        /* Make some bits on. */
         walb_bitmap_on(bmp, 0);
         walb_bitmap_on(bmp, 1);
-        walb_bitmap_print(bmp);
-        
-        walb_bitmap_off(bmp, 1);
-        walb_bitmap_print(bmp);
-        
-        walb_bitmap_clear(bmp);
-        walb_bitmap_print(bmp);
+        walb_bitmap_on(bmp, 2);
+        walb_bitmap_on(bmp, 7);
+        walb_bitmap_on(bmp, 8);
+        walb_bitmap_on(bmp, 9);
+        walb_bitmap_on(bmp, size - 1);
+        walb_bitmap_on(bmp, size - 2);
+        walb_bitmap_on(bmp, size - 3);
 
+        walb_bitmap_print(bmp);
+        ASSERT(walb_bitmap_get(bmp, 0));
+        ASSERT(walb_bitmap_get(bmp, 1));
+        ASSERT(walb_bitmap_get(bmp, 2));
+        ASSERT(walb_bitmap_get(bmp, 7));
+        ASSERT(walb_bitmap_get(bmp, 8));
+        ASSERT(walb_bitmap_get(bmp, 9));
+        ASSERT(walb_bitmap_get(bmp, size - 1));
+        ASSERT(walb_bitmap_get(bmp, size - 2));
+        ASSERT(walb_bitmap_get(bmp, size - 3));
+
+        /* Make all bits on. */
         for (i = 0; i < size; i ++) {
                 walb_bitmap_on(bmp, i);
         }
         walb_bitmap_print(bmp);
-        ret = walb_bitmap_is_all_on(bmp);
-        printf("is_all_on: %d\n", ret);
-        assert(ret == 1);
+        ASSERT(walb_bitmap_is_all_on(bmp));
         
+        walb_bitmap_off(bmp, 0);
+        walb_bitmap_off(bmp, 1);
         walb_bitmap_off(bmp, 2);
+        walb_bitmap_off(bmp, 7);
+        walb_bitmap_off(bmp, 8);
+        walb_bitmap_off(bmp, 9);
+        walb_bitmap_off(bmp, size - 1);
+        walb_bitmap_off(bmp, size - 2);
+        walb_bitmap_off(bmp, size - 3);
+
         walb_bitmap_print(bmp);
-        ret = walb_bitmap_is_all_on(bmp);
-        printf("is_all_on: %d\n", ret);
-        assert(ret == 0);
-        
+        ASSERT(! walb_bitmap_get(bmp, 0));
+        ASSERT(! walb_bitmap_get(bmp, 1));
+        ASSERT(! walb_bitmap_get(bmp, 2));
+        ASSERT(! walb_bitmap_get(bmp, 7));
+        ASSERT(! walb_bitmap_get(bmp, 8));
+        ASSERT(! walb_bitmap_get(bmp, 9));
+        ASSERT(! walb_bitmap_get(bmp, size - 1));
+        ASSERT(! walb_bitmap_get(bmp, size - 2));
+        ASSERT(! walb_bitmap_get(bmp, size - 3));
+
+        /* Clear the bitmap. */
         walb_bitmap_clear(bmp);
-        ret = walb_bitmap_is_all_off(bmp);
-        printf("is_all_on: %d\n", ret);
-        assert(ret == 1);
+        walb_bitmap_print(bmp);
+        ASSERT(walb_bitmap_is_all_off(bmp));
         
-        walb_bitmap_on(bmp, 2);
-        ret = walb_bitmap_is_all_off(bmp);
-        printf("is_all_on: %d\n", ret);
-        assert(ret == 0);
-        
+        /* Free the bitmap. */
         walb_bitmap_free(bmp);
 }
 
-
 int main()
 {
-        test(128);
-        test(127);
-        test(129);
+        test(64);
+        test(64 - 1);
+        test(64 + 1);
 
 	return 0;
 }
