6365b61ce4a8b834fa83d2e15185ef581563e9bc
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2012-01-24 16:27:02 +0900

    add wrapblk module.

---
+++ module/wrapblk.h
@@ -0,0 +1,14 @@
+/**
+ * wrapblk.h - Definitions for wrapblk.c.
+ *
+ * @author HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
+ */
+#ifndef _WRAPBLK_H_KERNEL
+#define _WRAPBLK_H_KERNEL
+
+#define WRAPBLK_NAME "wrapblk"
+
+
+
+
+#endif /* _WRAPBLK_H_KERNEL */
