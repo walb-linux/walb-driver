96d4d9c9befa5ce7fe5106a0b230b9c07cd384e2
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2012-02-07 11:39:51 +0900

    add a bit comment.

--- module/simple_blk_bio.h
+++ module/simple_blk_bio.h
@@ -14,9 +14,13 @@
  * Global functions prototype.
  *******************************************************************************/
 
+/* Make requrest for simpl_blk_bio_* modules. */
 int simple_blk_bio_make_request(struct request_queue *q, struct bio *bio);
+/* Create private data for sdev. */
 bool create_private_data(struct simple_blk_dev *sdev);
+/* Destroy private data for ssev. */
 void destroy_private_data(struct simple_blk_dev *sdev);
+/* Customize sdev after register before start. */
 void customize_sdev(struct simple_blk_dev *sdev);
 
 #endif /* _WALB_SIMPLE_BLK_BIO_H_KERNEL */
