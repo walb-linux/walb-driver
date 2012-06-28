4a5006f0465dae8c5ce72ab66cf8917a362f43c7
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2012-06-28 18:40:50 +0900

    add several methods of task serialization.

--- module/test_serialized_task.c
+++ module/test_serialized_task.c
@@ -17,6 +17,7 @@
 #include <linux/spinlock.h>
 #include <linux/kthread.h>
 #include <linux/wait.h>
+#include <linux/mutex.h>
 
 #include "walb/common.h"
 #include "walb/util.h"
@@ -31,90 +32,218 @@ struct workqueue_struct *wq_normal_;
 struct test_work
 {
 	struct work_struct work;
-	struct completion done;
-	struct test_work *next;
+	struct list_head list; /* list entry */
+	unsigned int *countp;
 	unsigned int cpuid; /* id of the cpu that enqueues the task. */
 };
 
 typedef void (test_work_task_fn)(struct work_struct *work);
 typedef struct test_work * (create_test_work_fn)(gfp_t gfp_mask);
 
+/* For benchmark_normal_list. */
+LIST_HEAD(test_work_list_);
+
+/* For benchmak_normal_mutex and benchmark_normal_list. */
+DEFINE_MUTEX(mutex_);
+
+struct thread_data
+{
+	unsigned int id;
+	struct task_struct *tsk;
+	wait_queue_head_t wait_q;
+	struct completion done;
+	const char *name;
+	void (*bench)(unsigned int n_tasks, unsigned int *countp);
+	unsigned int n_tasks;
+	unsigned int *countp;
+};
 
 /*******************************************************************************
  * Static functions prototype.
  *******************************************************************************/
 
+static int worker(void *data);
+static void run_benchmark(
+	unsigned int n_threads,
+	const char *name,
+	void (*bench)(unsigned int n_tasks, unsigned int *countp),
+	unsigned int n_tasks);
+
 static void test_work_task_single(struct work_struct *work);
-static void test_work_task_normal(struct work_struct *work);
-static void benchmark_single(unsigned int n_tasks);
-static void benchmark_normal(unsigned int n_tasks);
+static void test_work_task_normal_list(struct work_struct *work);
+static void test_work_task_normal_mutex(struct work_struct *work);
+static void benchmark_single(unsigned int n_tasks, unsigned int *countp);
+static void benchmark_normal(unsigned int n_tasks, unsigned int *countp);
+static void benchmark_normal_list(unsigned int n_tasks, unsigned int *countp);
+static void benchmark_normal_mutex(unsigned int n_tasks, unsigned int *countp);
 static void malloc_and_free(unsigned int n_tasks);
 
-static struct test_work* create_test_work_single(gfp_t gfp_mask);
-static struct test_work* create_test_work_normal(gfp_t gfp_mask);
+static struct test_work* create_test_work(gfp_t gfp_mask);
 static void destroy_test_work(struct test_work *test_work);
 
 static void init_workqueue(void);
 static void fin_workqueue(void);
 
-
 /*******************************************************************************
  * Static functions definition.
  *******************************************************************************/
+
+static int worker(void *data)
+{
+	struct thread_data *tdata = (struct thread_data *)data;
+	tdata->bench(tdata->n_tasks, tdata->countp);
+	complete(&tdata->done);
+	return 0;
+}
+
+static void run_benchmark(
+	unsigned int n_threads,
+	const char *name,
+	void (*bench)(unsigned int n_tasks, unsigned int *countp),
+	unsigned int n_tasks)
+{
+	unsigned int i;
+	struct thread_data *tdata;
+	struct timespec bgn_ts, end_ts, sub_ts;
+	unsigned int count = 0;
+	ASSERT(n_threads > 0);
+
+	tdata = kmalloc(sizeof(struct thread_data) * n_threads, GFP_KERNEL);
+	ASSERT(tdata);
+
+	getnstimeofday(&bgn_ts);
+	for (i = 0; i < n_threads; i++) {
+		tdata[i].id = i;
+		init_completion(&tdata[i].done);
+		init_waitqueue_head(&tdata[i].wait_q);
+		tdata[i].bench = bench;
+		tdata[i].n_tasks = n_tasks;
+		tdata[i].countp = &count;
+		tdata[i].tsk = kthread_run(worker, &tdata[i], "test_worker%u", i);
+	}
+	for (i = 0; i < n_threads; i++) {
+		wait_for_completion(&tdata[i].done);
+	}
+	getnstimeofday(&end_ts);
+	sub_ts = timespec_sub(end_ts, bgn_ts);
+	kfree(tdata);
+	LOGn("%s %u %ld.%09ld\n", name, count, sub_ts.tv_sec, sub_ts.tv_nsec);
+}
 
 static void test_work_task_single(struct work_struct *work)
 {
 	struct test_work *w = container_of(work, struct test_work, work);
 	LOGd_("enqueue %u dequeue %u\n", w->cpuid, raw_smp_processor_id());
+	(*w->countp)++;
 	destroy_test_work(w);
 }
 
-static void test_work_task_normal(struct work_struct *work)
+static void test_work_task_normal_list(struct work_struct *work)
+{
+	struct test_work *w;
+	bool should_loop = true;
+
+	while (true) {
+		
+		mutex_lock(&mutex_);
+		if (list_empty(&test_work_list_)) {
+			w = NULL;
+			should_loop = false;
+		} else {
+			w = list_first_entry(&test_work_list_, struct test_work, list);
+		}
+		mutex_unlock(&mutex_);
+
+		if (!should_loop) {
+			break;
+		}
+		
+		LOGd_("enqueue %u dequeue %u\n", w->cpuid, raw_smp_processor_id());
+#if 0
+		if (*w->countp % 100000 == 0) {
+			LOGn("count %u\n", *w->countp);
+		}
+#endif
+		(*w->countp)++;
+		
+		mutex_lock(&mutex_);
+		list_del(&w->list);
+		mutex_unlock(&mutex_);
+
+		destroy_test_work(w);
+	}
+}
+
+static void test_work_task_normal_mutex(struct work_struct *work)
 {
 	struct test_work *w = container_of(work, struct test_work, work);
+	mutex_lock(&mutex_);
 	LOGd_("enqueue %u dequeue %u\n", w->cpuid, raw_smp_processor_id());
-	wait_for_completion(&w->done);
-	if (w->next) {
-		complete(&w->next->done);
-	}
+	(*w->countp)++;
+	mutex_unlock(&mutex_);
 	destroy_test_work(w);
 }
 
-static void benchmark_normal(unsigned int n_tasks)
+static void benchmark_normal_list(
+	unsigned int n_tasks, unsigned int *countp)
 {
 	unsigned int i;
-	struct timespec bgn_ts, end_ts, sub_ts;
-	struct test_work *w, *prev;
-	
+	struct test_work *w;
+	bool is_empty;
 	ASSERT(n_tasks > 0);
-
-	getnstimeofday(&bgn_ts);
-	prev = NULL;
-	for (i = 0; i < n_tasks; i ++) {
-
-		w = create_test_work_normal(GFP_KERNEL);
-		ASSERT(w);
-		INIT_WORK(&w->work, test_work_task_normal);
-		if (prev) {
-			prev->next = w;
-			queue_work(wq_normal_, &prev->work);
-		} else {
-			/* Kick the first task. */
-			complete(&w->done);
-		}
-		prev = w;
-	}
-	ASSERT(w);
-	queue_work(wq_normal_, &w->work);
 	
+	for (i = 0; i < n_tasks; i ++) {
+		w = create_test_work(GFP_KERNEL);
+		w->countp = countp;
+		ASSERT(w);
+		INIT_WORK(&w->work, test_work_task_normal_list);
+		mutex_lock(&mutex_);
+		is_empty = list_empty(&test_work_list_);
+		list_add_tail(&w->list, &test_work_list_);
+		mutex_unlock(&mutex_);
+		if (is_empty) {
+			queue_work(wq_normal_, &w->work);
+		}
+	}
 	flush_workqueue(wq_normal_);
-	getnstimeofday(&end_ts);
-
-	sub_ts = timespec_sub(end_ts, bgn_ts);
-	LOGn("wq_normal %ld.%09ld\n", sub_ts.tv_sec, sub_ts.tv_nsec);
 }
 
-static void benchmark_single(unsigned int n_tasks)
+static void benchmark_normal_mutex(
+	unsigned int n_tasks, unsigned int *countp)
+{
+	unsigned int i;
+	struct test_work *w;
+	ASSERT(n_tasks > 0);
+
+	for (i = 0; i < n_tasks; i ++) {
+		w = create_test_work(GFP_KERNEL);
+		w->countp = countp;
+		ASSERT(w);
+		INIT_WORK(&w->work, test_work_task_normal_mutex);
+		queue_work(wq_normal_, &w->work);
+	}
+	flush_workqueue(wq_normal_);
+}
+
+static void benchmark_normal(
+	unsigned int n_tasks, unsigned int *countp)
+{
+	unsigned int i;
+	struct test_work *w;
+	ASSERT(n_tasks > 0);
+
+	for (i = 0; i < n_tasks; i ++) {
+		w = create_test_work(GFP_KERNEL);
+		w->countp = countp;
+		ASSERT(w);
+		INIT_WORK(&w->work, test_work_task_single);
+		queue_work(wq_normal_, &w->work);	
+	}
+	flush_workqueue(wq_normal_);
+}
+
+static void benchmark_single(
+	unsigned int n_tasks, unsigned int *countp)
 {
 	unsigned int i;
 	struct timespec bgn_ts, end_ts, sub_ts;
@@ -124,7 +253,8 @@ static void benchmark_single(unsigned int n_tasks)
 
 	getnstimeofday(&bgn_ts);
 	for (i = 0; i < n_tasks; i ++) {
-		w = create_test_work_single(GFP_KERNEL);
+		w = create_test_work(GFP_KERNEL);
+		w->countp = countp;
 		ASSERT(w);
 		INIT_WORK(&w->work, test_work_task_single);
 		queue_work(wq_single_, &w->work);	
@@ -133,7 +263,7 @@ static void benchmark_single(unsigned int n_tasks)
 	getnstimeofday(&end_ts);
 
 	sub_ts = timespec_sub(end_ts, bgn_ts);
-	LOGn("wq_single %ld.%09ld\n", sub_ts.tv_sec, sub_ts.tv_nsec);
+	/* LOGn("wq_single %u %ld.%09ld\n", count, sub_ts.tv_sec, sub_ts.tv_nsec); */
 }
 
 static void malloc_and_free(unsigned int n_tasks)
@@ -146,7 +276,7 @@ static void malloc_and_free(unsigned int n_tasks)
 
 	getnstimeofday(&bgn_ts);
 	for (i = 0; i < n_tasks; i ++) {
-		w = create_test_work_single(GFP_KERNEL);
+		w = create_test_work(GFP_KERNEL);
 		ASSERT(w);
 		destroy_test_work(w);
 	}
@@ -156,22 +286,10 @@ static void malloc_and_free(unsigned int n_tasks)
 	LOGn("baseline %ld.%09ld\n", sub_ts.tv_sec, sub_ts.tv_nsec);
 }
 
-
-static struct test_work* create_test_work_normal(gfp_t gfp_mask)
+static struct test_work* create_test_work(gfp_t gfp_mask)
 {
 	struct test_work *w = kmalloc(sizeof(struct test_work), gfp_mask);
 	ASSERT(w);
-	init_completion(&w->done);
-	w->next = NULL;
-	w->cpuid = raw_smp_processor_id();
-	return w;
-}
-
-static struct test_work* create_test_work_single(gfp_t gfp_mask)
-{
-	struct test_work *w = kmalloc(sizeof(struct test_work), gfp_mask);
-	ASSERT(w);
-	init_completion(&w->done);
 	w->cpuid = raw_smp_processor_id();
 	return w;
 }
@@ -186,7 +304,7 @@ static void init_workqueue(void)
 {
 	wq_single_ = create_singlethread_workqueue("test_serialize_single");
 	ASSERT(wq_single_);
-	wq_normal_ = alloc_workqueue("test_serialize_normal", WQ_MEM_RECLAIM, 0);
+	wq_normal_ = alloc_workqueue("test_serialize_normal", WQ_MEM_RECLAIM, 1);
 	ASSERT(wq_normal_);
 }
 
@@ -202,13 +320,17 @@ static void fin_workqueue(void)
 
 static int __init test_init(void)
 {
-	unsigned int n_tasks = 1000000;
-	/* unsigned int n_tasks = 20; */
+	unsigned int n_threads = 4;
+	unsigned int n_tasks = 1000000 / n_threads;
 	
         init_workqueue();
-	benchmark_single(n_tasks);
-	benchmark_normal(n_tasks);
+
+	run_benchmark(n_threads, "single", benchmark_single, n_tasks);
+	run_benchmark(n_threads, "normal", benchmark_normal, n_tasks);
+	run_benchmark(n_threads, "normal_l", benchmark_normal_list, n_tasks);
+	run_benchmark(n_threads, "normal_m", benchmark_normal_mutex, n_tasks);
 	malloc_and_free(n_tasks);
+	
         fin_workqueue();
 
         return -1;
