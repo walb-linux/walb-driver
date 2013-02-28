09f123071dfff7ef6299c43f41ac1fb3088c99b9
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2013-02-28 16:13:57 +0900

    wlog: add lack of logs check to test_wlog script.

--- tool/wlog/test_wlog.sh
+++ tool/wlog/test_wlog.sh
@@ -21,11 +21,18 @@ format_ldev()
   losetup $LOOP0 $LDEV
   losetup $LOOP1 ${DDEV}.0
   $CTL format_ldev --ldev $LOOP0 --ddev $LOOP1
-  RING_BUFFER_SIZE=$(./wlinfo $LOOP0 |grep ringBufferSize |awk '{print $2}')
+  RING_BUFFER_SIZE=$(${BIN}/wlinfo $LOOP0 |grep ringBufferSize |awk '{print $2}')
   echo $RING_BUFFER_SIZE
   sleep 1
   losetup -d $LOOP0
   losetup -d $LOOP1
+}
+
+echo_wlog_value()
+{
+  local wlogFile=$1
+  local keyword=$2
+  $CTL show_wlog < $wlogFile |grep $keyword |awk '{print $2}'
 }
 
 #
@@ -33,6 +40,10 @@ format_ldev()
 #
 format_ldev
 ${BIN}/wlgen -s 32M -z 16M --maxPackSize 4M -o ${WLOG}.0
+#${BIN}/wlgen -s 32M -z 16M --minIoSize 512 --maxIoSize 512 --maxPackSize 1M -o ${WLOG}.0
+endLsid0=$(echo_wlog_value ${WLOG}.0 end_lsid_really:)
+nPacks0=$(echo_wlog_value ${WLOG}.0 n_packs:)
+totalPadding0=$(echo_wlog_value ${WLOG}.0 total_padding_size:)
 cp ${DDEV}.0 ${DDEV}.0z
 ${BIN}/wlredo ${DDEV}.0 < ${WLOG}.0
 ${BIN}/wlredo ${DDEV}.0z --zerodiscard < ${WLOG}.0
@@ -67,6 +78,27 @@ restore_test()
   $CTL cat_wldev --wldev $LOOP0 > ${WLOG}.2
   sleep 1
   losetup -d $LOOP0
+  if [ $invalidLsid -lt 0 ]; then
+    local endLsid0a=$(expr $endLsid0 + $lsidDiff - $nPacks0 - $totalPadding0)
+    local endLsid1=$(echo_wlog_value ${WLOG}.1 end_lsid_really:)
+    local endLsid2=$(echo_wlog_value ${WLOG}.2 end_lsid_really:)
+    local nPacks1=$(echo_wlog_value ${WLOG}.1 n_packs:)
+    local nPacks2=$(echo_wlog_value ${WLOG}.2 n_packs:)
+    local totalPadding1=$(echo_wlog_value ${WLOG}.1 total_padding_size:)
+    local totalPadding2=$(echo_wlog_value ${WLOG}.2 total_padding_size:)
+    local endLsid1a=$(expr $endLsid1 - $nPacks1 - $totalPadding1)
+    local endLsid2a=$(expr $endLsid2 - $nPacks2 - $totalPadding2)
+    if [ $endLsid0a -ne $endLsid1a ]; then
+      echo endLsid0a $endLsid0a does not equal to endLsid1a $endLsid1a
+      echo "TEST${testId}_FAILURE"
+      exit 1
+    fi
+    if [ $endLsid0a -ne $endLsid2a ]; then
+      echo endLsid0a $endLsid0a does not equal to endLsid2a $endLsid2a
+      echo "TEST${testId}_FAILURE"
+      exit 1
+    fi
+  fi
   ${BIN}/bdiff -b 512 ${WLOG}.1 ${WLOG}.2
   if [ $? -ne 0 ]; then
     echo ${WLOG}.1 and ${WLOG}.2 differ.
@@ -89,38 +121,38 @@ restore_test()
   if [ $invalidLsid -eq -1 ]; then
     ${BIN}/bdiff -b 512 ${DDEV}.0 ${DDEV}.1
     if [ $? -ne 0 ]; then
-      echo "failed: ./bdiff -b 512 ${DDEV}.0 ${DDEV}.1"
+      echo "failed: ${BIN}/bdiff -b 512 ${DDEV}.0 ${DDEV}.1"
       echo "TEST${testId}_FAILURE"
       exit 1
     fi
     ${BIN}/bdiff -b 512 ${DDEV}.0 ${DDEV}.2
     if [ $? -ne 0 ]; then
-      echo "failed: ./bdiff -b 512 ${DDEV}.0 ${DDEV}.2"
+      echo "failed: ${BIN}/bdiff -b 512 ${DDEV}.0 ${DDEV}.2"
       echo "TEST${testId}_FAILURE"
       exit 1
     fi
     ${BIN}/bdiff -b 512 ${DDEV}.0 ${DDEV}.3
     if [ $? -ne 0 ]; then
-      echo "failed: ./bdiff -b 512 ${DDEV}.0 ${DDEV}.3"
+      echo "failed: ${BIN}/bdiff -b 512 ${DDEV}.0 ${DDEV}.3"
       echo "TEST${testId}_FAILURE"
       exit 1
     fi
     ${BIN}/bdiff -b 512 ${DDEV}.0z ${DDEV}.1z
     if [ $? -ne 0 ]; then
-      echo "failed: ./bdiff -b 512 ${DDEV}.0z ${DDEV}.1z"
+      echo "failed: ${BIN}/bdiff -b 512 ${DDEV}.0z ${DDEV}.1z"
       echo "TEST${testId}_FAILURE"
       exit 1
     fi
   else
     ${BIN}/bdiff -b 512 ${DDEV}.1 ${DDEV}.2
     if [ $? -ne 0 ]; then
-      echo "failed: ./bdiff -b 512 ${DDEV}.1 ${DDEV}.2"
+      echo "failed: ${BIN}/bdiff -b 512 ${DDEV}.1 ${DDEV}.2"
       echo "TEST${testId}_FAILURE"
       exit 1
     fi
     ${BIN}/bdiff -b 512 ${DDEV}.1 ${DDEV}.3
     if [ $? -ne 0 ]; then
-      echo "failed: ./bdiff -b 512 ${DDEV}.1 ${DDEV}.3"
+      echo "failed: ${BIN}/bdiff -b 512 ${DDEV}.1 ${DDEV}.3"
       echo "TEST${testId}_FAILURE"
       exit 1
     fi
@@ -129,8 +161,9 @@ restore_test()
 
 restore_test 3 $(expr $RING_BUFFER_SIZE - 1) -1
 restore_test 4 $(expr $RING_BUFFER_SIZE - 2) -1
-restore_test 5 0 1024 #512KB
-restore_test 6 0 8192 #4MB
+restore_test 5 $(expr $RING_BUFFER_SIZE - 1024) -1
+restore_test 6 0 1024 #512KB
+restore_test 7 0 8192 #4MB
 
 echo TEST_SUCCESS
 exit 0
