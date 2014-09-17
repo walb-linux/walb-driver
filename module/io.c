b9d459bb84b04e706496ae03ae089eb1acddee64
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2014-09-17 11:33:22 +0900

    disable a log.

--- module/io.c
+++ module/io.c
@@ -235,7 +235,7 @@ static void bio_entry_end_io(struct bio *bio, int error)
 
 	if (!uptodate) {
 		UNUSED const unsigned int devt = bio->bi_bdev->bd_dev;
-		LOGd("BIO_UPTODATE is false"
+		LOG_("BIO_UPTODATE is false"
 			" (dev %u:%u rw %lu pos %" PRIu64 " len %u).\n"
 			, MAJOR(devt), MINOR(devt)
 			, bio->bi_rw
