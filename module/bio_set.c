2cb3bc2fd7ec01ba440222ca34fde8307d015f01
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2018-08-28 22:44:30 +0900

    for-4.18: use bioset_init()/bioset_exit() instead of bioset_create()/bioset_free().

--- module/bio_set.c
+++ module/bio_set.c
@@ -7,12 +7,15 @@
  */
 #include "check_kernel.h"
 #include <linux/bio.h>
+#include <linux/slab.h>
+#include <linux/version.h>
 #include "bio_set.h"
 
 
 struct bio_set *walb_bio_set_ = NULL;
 
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0)
 bool walb_bio_set_init(void)
 {
 	if (walb_bio_set_)
@@ -36,8 +39,48 @@ error:
 
 void walb_bio_set_exit(void)
 {
-	if (walb_bio_set_) {
-		bioset_free(walb_bio_set_);
-		walb_bio_set_ = NULL;
-	}
+	if (!walb_bio_set_)
+		return;
+
+	bioset_free(walb_bio_set_);
+	walb_bio_set_ = NULL;
 }
+
+#else /* #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0) */
+
+bool walb_bio_set_init(void)
+{
+	if (walb_bio_set_)
+		return true;
+
+	walb_bio_set_ = kzalloc(sizeof(struct bio_set), GFP_KERNEL);
+	if (!walb_bio_set_)
+		goto error0;
+
+	/*
+	 * BIOSET_NEED_BVECS is not set,
+	 * so you can use walb_bio_set_ for bio_clone_fast() only.
+	 * Do not use bio_clone_bioset().
+	 */
+	if (bioset_init(walb_bio_set_, BIO_POOL_SIZE, 0, BIOSET_NEED_RESCUER))
+		goto error1;
+
+	return true;
+error1:
+	kfree(walb_bio_set_);
+	walb_bio_set_ = NULL;
+error0:
+	return false;
+}
+
+
+void walb_bio_set_exit(void)
+{
+	if (!walb_bio_set_)
+		return;
+
+	bioset_exit(walb_bio_set_);
+	kfree(walb_bio_set_);
+	walb_bio_set_ = NULL;
+}
+#endif /* #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0) */
